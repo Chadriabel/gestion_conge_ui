b5635934b45659d5176cdcc32ae560e0aed8b9a3
Chadriabel <chadryndayiziga266@gmail.com>
2024-01-23 18:46:51 +0200

    init project

---
+++ src/views/LoginView.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+<form>
+<input type="text" placeholder="username" v-model="username">
+<input type="password" placeholder="password" v-model="password">
+<button type="submit">login</button>
+</form>
+<p>user name ={{username}}</p>
+<p>password ={{password}}</p>
+<div>vos identifient sont : <strong></strong></div>
+
+</div>
+</template>
+
+<script>
+export default{
+        data(){
+                return{
+                        username:"chadry",
+                        password:"7"
+                        
+                }
+        },
+        methods:{
+                login(){
+
+                },
+                performlogin(){
+                        this.result=""
+                }
+        
+                
+        }
+
+}
+
+</script>
+<style scoped>
+</style>
